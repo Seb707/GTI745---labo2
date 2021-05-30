1f2f11c7aa2094df43cbad183dd4b7e05dae6e87
Sebastien <c.sebastien707@gmail.com>
2021-05-30 17:30:17 -0400

    update of textures. Skybox. dorr fade. UI elements. To do: Enemy response and lvl design

--- Roll_A_Ball/Assets/Scripts/PlayerController.cs
+++ Roll_A_Ball/Assets/Scripts/PlayerController.cs
@@ -9,28 +9,117 @@ public class PlayerController : MonoBehaviour
     public float speed = 0;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
+    public TextMeshProUGUI speedTextObject;
+    public TextMeshProUGUI ObjectiveTextObject;
+    public float fadeSpeed;
 
+    private bool fadeIn, fadeOut;
+    private int objective = 1;
+    private GameObject door;
     private Rigidbody rb;
     private int count;
+    private int totalPickUps;
     private float movementX;
     private float movementY;
+
+    //private int lifeCounter = 4;
+    //public Image[] hearts;
+    //public Sprite fullHeart;
+    //public Sprite emptyHeart;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
+        totalPickUps = 9;
         SetCountText();
         winTextObject.SetActive(false);
+        SetSpeedText();
+        SetObjectifText();
+        door = GameObject.Find("Door");
+
+    }
+
+    void Update() {
+        SetSpeedText();
+        SetObjectifText();
+        //print("update");
+        if (fadeOut)
+        {
+            //j.GetComponent<MeshRenderer>().material.color = Color.Lerp(obj.GetComponent<MeshRenderer>().material.color, alphaColor, timeToFade * Time.deltaTime);
+            Color objectColor = door.GetComponent<Renderer>().material.color;
+            float fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            door.GetComponent<Renderer>().material.color = objectColor;
+
+            if (objectColor.a <= 0)
+            {
+                print("Fading out finished");
+                fadeOut = false;
+                door.SetActive(false);
+            }
+        }
+        else if (fadeIn) {
+            //j.GetComponent<MeshRenderer>().material.color = Color.Lerp(obj.GetComponent<MeshRenderer>().material.color, alphaColor, timeToFade * Time.deltaTime);
+            Color objectColor = door.GetComponent<Renderer>().material.color;
+            float fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            door.GetComponent<Renderer>().material.color = objectColor;
+
+            if (objectColor.a > 0)
+            {
+                door.SetActive(true);
+            }
+            if (objectColor.a >= 1)
+            {
+                print("Fading in finished");
+                fadeIn = false;
+            }
+        }
+    }
+
+    void SetObjectifText()
+    {
+        
+        
+
+        switch (objective)
+        {
+            case 1:
+                ObjectiveTextObject.text = "Veuillez explorer la zone et ramasser les cube jaune!";
+                break;
+            case 2:
+                ObjectiveTextObject.text = "La porte c'est ouverte!";
+                break;
+            case 3:
+                ObjectiveTextObject.text = "Ball Speed: ";
+                break;
+            default:
+
+                break;
+        }
+
     }
 
     void SetCountText()
     {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 8)
+        countText.text = "Count: " + count.ToString()+"/"+ totalPickUps.ToString();
+        if (count >= 9)
         {
             winTextObject.SetActive(true);
         }
+    }
+
+    void SetSpeedText()
+    {
+        float speed = rb.velocity.magnitude;
+        if (speed < 0.1) {
+            rb.velocity = Vector3.zero;
+            speed = rb.velocity.magnitude;
+        }
+        speedTextObject.text = "Ball Speed: " + speed.ToString();
     }
 
     private void OnMove(InputValue movementValue)
@@ -48,12 +137,32 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movement * speed);
     }
 
+    private void OnCollisionEnter(Collision col) {
+
+        if (col.gameObject.tag == "Enemy") {
+            door.SetActive(true);
+        }
+        Debug.LogFormat("{0} collision enter: {1}", this, col.gameObject);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+
+        if (other.gameObject.name == "doorHitBox")
+        {
+            print("Woohoo");
+            fadeIn = true;
+        }
+
         if (other.gameObject.CompareTag("PickUp"))
         { 
             other.gameObject.SetActive(false);
             count++;
+
+            if (count == 1){
+                print("fadeOut true");
+                fadeOut = true;
+            }
 
             SetCountText();
         }
---
+++ Roll_A_Ball/Assets/waypointTravel.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waypointTravel : MonoBehaviour
+{
+    //public int nbWaypoints;
+    public GameObject[] waypoints;
+    int current = 0;
+    float rotSpeed;
+    public float speed ;
+    float WPradius = 0.2f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
+        {
+            current++;
+            if (current >= waypoints.Length)
+            {
+                current = 0;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+        
+    }
+}
